f5818dc655049f97f32a5679c625849a1fcc43a0
Kerolos Zaki <52022280+ItsKerolos@users.noreply.github.com>
2020-05-24 18:04:36 +0200

    Show more of the song's metadata inside the menu

--- mpris.1l.1s.sh
+++ mpris.1l.1s.sh
@@ -6,13 +6,30 @@ PLAY_PAUSE="$COMMAND_BASE play-pause"
 NEXT="$COMMAND_BASE next"
 PREVIOUS="$COMMAND_BASE previous"
 
+STATE=$($COMMAND_BASE status)
+
 ARTIST=$($COMMAND_BASE metadata xesam:artist)
 SONG_TITLE=$($COMMAND_BASE metadata xesam:title)
 
 ART_URL=$($COMMAND_BASE metadata mpris:artUrl)
 
+# Workaround: Spotify not caring to update their client on linux.
+ART_URL="${ART_URL/"open.spotify.com"/"i.scdn.co"}"
+
+ART_IMG=$(curl -s "$ART_URL" | base64 -w 0)
+
 # get current hour (24 clock format i.e. 0-23)
 hour=$(date +"%H")
+
+# PLAY/PAUSE FORMAT
+
+if [ "$STATE" == "Playing" ]; then
+    STATE="Pause"
+else
+    STATE="Play"
+fi
+
+# GREET FORMAT
 
 # if it is midnight to midafternoon will say G'morning
 if [ $hour -ge 0 -a $hour -lt 12 ]; then
@@ -24,6 +41,8 @@ else # it is good evening till midnight
   greet="Good evening, $USER."
 fi
 
+# TITLE FORMAT
+
 if [[ -z "$SONG_TITLE" ]]; then
   TITLE="$greet"
 elif [[ -z "$ARTIST" ]]; then
@@ -32,9 +51,13 @@ else
   TITLE="$ARTIST - $SONG_TITLE"
 fi
 
+# ARGOS OUTPUT
+
 echo "$TITLE"
 echo "---"
-echo "Play - Pause | bash='$PLAY_PAUSE' terminal=false refresh=false"
+echo "<b>$ARTIST</b>\n<b>$SONG_TITLE</b> | image='$ART_IMG' imageWidth=56 imageHeight=56"
+echo "---"
+echo "$STATE | bash='$PLAY_PAUSE' terminal=false refresh=true"
 echo "---"
 echo "Next | bash='$NEXT' terminal=false refresh=true"
 echo "Previous | bash='$PREVIOUS' terminal=false refresh=true"
